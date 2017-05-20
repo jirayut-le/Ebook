31412171016781b03b07803306747c9e19b9e321
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-05-21 00:31:16 +0700

    add user class of customer

--- app/src/main/java/com/example/delusion/ebook/User.java
+++ app/src/main/java/com/example/delusion/ebook/User.java
@@ -1,8 +1,41 @@
 package com.example.delusion.ebook;
 
+import java.util.ArrayList;
+
 /**
  * Created by macbook on 5/20/2017 AD.
  */
 
 public class User {
+    private double fund;
+    private ArrayList<Book> ownEbook;
+    private ArrayList<Book> lastBought;
+
+    public User(){
+        this.fund = 100;
+        this.ownEbook = new ArrayList<>();
+        this.lastBought = new ArrayList<>();
+    }
+
+    public void addFund(String fund){
+        this.fund += Integer.parseInt(fund);
+    }
+
+    public void buyEbook(ArrayList<Book> buyList){
+        for(Book b : buyList){
+            this.fund -= b.getPrice();
+        }
+        ownEbook.addAll(buyList);
+        lastBought.addAll(buyList);
+    }
+
+    public void refund(){
+        for(Book b : lastBought){
+            fund += b.getPrice();
+        }
+        ownEbook.removeAll(lastBought);
+        lastBought.clear();
+    }
+
+
 }
