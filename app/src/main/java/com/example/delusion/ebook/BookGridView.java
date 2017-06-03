ce6287ba776700601f1e52612706da08be18689e
Jirayut Leeupathumvong <jirayut_32547@hotmail.com>
2017-06-03 22:13:53 +0700

    add interface of gridView

---
+++ app/src/main/java/com/example/delusion/ebook/BookGridView.java
@@ -0,0 +1,10 @@
+package com.example.delusion.ebook;
+
+/**
+ * Created by macbook on 6/3/2017 AD.
+ */
+
+public interface BookGridView {
+    void setBookImg(int pos);
+    void setBookName(int pos);
+}
